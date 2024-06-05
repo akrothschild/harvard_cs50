61de49f21aed591dd0fb1facc4b0de125516fddc
Alexander <51855086+akrothschild@users.noreply.github.com>
2024-06-05 13:49:14 +0200

    add lib

---
+++ library.c
@@ -0,0 +1,8 @@
+#include "library.h"
+
+#include "libcs50-11.0.2/src/cs50.h"
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef HARVARD_CS50_LIBRARY_H
+#define HARVARD_CS50_LIBRARY_H
+
+void hello(void);
+
+#endif //HARVARD_CS50_LIBRARY_H
